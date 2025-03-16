df3c84d96bc062ffbab325dffd4a637b34abba9d
Amani Khalid <amanikhalidd@gmail.com>
2025-03-15 22:36:23 -0700

    simple calculator

--- Program.cs
+++ Program.cs
@@ -4,7 +4,184 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //    //1. Even or Odd
+
+            //    int num1;
+            //    Console.WriteLine("Enter a Number");
+            //    num1 = int.Parse(Console.ReadLine());
+
+            //    if (num1 % 2 == 0)
+            //    {
+            //        Console.WriteLine(num1 + "is an Even Number");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(num1 + "is an Odd Number");
+            //    }
+            ////-----------------------------------------------------------
+
+            //2. Largest of Three Numbers
+            //int num1, num2, num3, large;
+            //Console.WriteLine("Enter First Number:");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Second Number");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Third Number");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 > num2 && num1 > num3)
+            //{
+            //    large = num1;
+            //}
+            //else if (num2 > num1 && num2 > num3)
+            //{
+            //    large = num2;
+            //}
+            //else
+            //{
+            //    large = num3;
+            //}
+            //Console.WriteLine("The Largest Number is " + large);
+            ////--------------------------------------------------------------
+
+            //3. Temperature Converter
+            //double temp, Fahrenheit;
+            //Console.WriteLine("Enter Temperature in Celsius");
+            //double Celsius = double.Parse(Console.ReadLine());
+
+            //Fahrenheit = (Celsius * 9 / 5) + 32;
+            //Console.WriteLine("Temperature in Fahrenheit is " + Fahrenheit); 
+
+            ////----------------------------------------------------------------
+
+            //4. Simple Discount Calculator
+            //float Price, FinalPrice, Discount;
+            //Console.WriteLine("Enter The Price");
+            //Price = float.Parse(Console.ReadLine());
+
+            //if (Price > 100)
+            //{
+            //    Discount = (Price / 100) *10;
+            //    FinalPrice = Price - Discount;
+            //    Console.WriteLine("Final Price is "+ FinalPrice);
+
+            //}
+            //else
+            //{
+            //    FinalPrice = Price;
+            //    Console.WriteLine("Final Price is "+ FinalPrice);
+            //}
+
+            ////-----------------------------------------------------------------
+
+            //5. Grading System
+            //float StudentMark;
+            //Console.WriteLine("Enter Your Score");
+            //StudentMark = float.Parse(Console.ReadLine());
+
+            //if (StudentMark >=90)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (StudentMark >=80)
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else if (StudentMark >=70)
+            //{
+            //    Console.WriteLine("C");
+            //}
+            //else if (StudentMark >=60)
+            //{
+            //    Console.WriteLine("D");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("F");
+            //}
+
+            ////---------------------------------------------------------------
+
+            //6. Swap Two Numbers
+            //int a, b,c;
+            //Console.WriteLine("Enter First Number");
+            //a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Second Number");
+            //b = int.Parse(Console.ReadLine());
+
+            //c = a;
+            //a = b;
+            //b = c;
+            //Console.WriteLine("After Swapping First Number = " + a + ", Second Number = " + b);
+
+
+            //---------------------------------------------(CSharpPart2Exercises)
+
+            //1. Simple Calculator (Switch Case)
+            char choice;
+            do
+            {
+                Console.Clear();
+                int OperationNumber, num1, num2;
+                Console.WriteLine("Enter The Operation Number:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+
+                OperationNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter First Number: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number: ");
+                num2 = int.Parse(Console.ReadLine());
+
+                double result = 0;
+                bool isActive = false;
+                switch (OperationNumber)
+                {
+                    case 1:
+                        result = num1 + num2;
+                        break;
+
+                    case 2:
+                        result = num1 - num2;
+                        break;
+
+                    case 3:
+                        result = num1 * num2;
+                        break;
+
+                    case 4:
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Division by Zero is not allowed");
+                            isActive = true;
+                        }
+
+                        else
+                        {
+                            result = num1 / num2;
+
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+
+                }
+                if (!isActive)
+                {
+                    Console.WriteLine(result);
+                }
+                Console.WriteLine("Do you want another Operation? Y / N");
+                choice = Console.ReadKey().KeyChar;
+
+
+
+            } while (choice == 'Y' || choice == 'y');
+            Console.WriteLine("\nSee You Next Time");
         }
+
+        //-------------------------------------------------------------------
     }
 }
+
