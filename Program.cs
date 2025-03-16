3c795aefb0c9be728c07b5260638754b141d9209
Amani Khalid <amanikhalidd@gmail.com>
2025-03-15 22:35:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseAssignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
